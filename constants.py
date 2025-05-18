72037b3df04439c626ce26ecb134d7c510e21e4a
jasperschroeder <schroejas@gmail.com>
2025-05-18 21:03:57 +0200

    Refactor ML utilities for LSTM and RNN models; add early stopping and MLflow integration
    - Enhanced `create_sequences_for_lstm` to `train_val_split_lstm_feature_data` for better data handling.
    - Introduced `build_seq_model` to support both LSTM and SimpleRNN architectures.
    - Added `get_best_model` function to retrieve the best model based on validation metrics.
    - Updated `run_experiment` to accommodate new model structure and logging parameters.
    - Created `constants.py` for configuration constants.
    - Updated `requirements.txt` to include `holidays` package.

---
+++ constants.py
@@ -0,0 +1,6 @@
+TRAIN_FRAC = 0.85
+VAL_FRAC = 0.1
+
+URL_DATA = 'https://storage.data.gov.my/healthcare/blood_donations_state.parquet'
+
+WINDOW_SIZE = 7
--- ml_utils.py
+++ ml_utils.py
@@ -1,50 +1,17 @@
 import numpy as np
 from tensorflow.keras.callbacks import EarlyStopping
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout, Input
-
+from keras.models import Model
+from keras.layers import LSTM, Dense, Dropout, Input, SimpleRNN, Concatenate
 import mlflow
-
+import absl.logging
+absl.logging.set_verbosity(absl.logging.ERROR)
 import warnings
-warnings.filterwarnings("ignore") 
-
-
-def create_sequences_for_lstm(data: np.ndarray, seq_length: int = 30):
-    X = [data[i:(i+seq_length)] for i in range(len(data) - seq_length)]
-    y = [data[i + seq_length][0] for i in range(len(data) - seq_length)]
-    return np.array(X), np.array(y)
-
-
-def create_train_val_test(X: np.ndarray, y: np.ndarray, train_frac: float, val_frac: float):
-    if train_frac + val_frac >= 1:
-        raise ValueError("train_frac + val_frac must be less than 1")
-    if not (0 < train_frac < 1) or not (0 < val_frac < 1):
-        raise ValueError("train_frac and val_frac must be between 0 and 1")
-    
-    train_size = int(len(X) * train_frac)
-    val_size = int(len(X) * val_frac)
-    
-    X_train, y_train = X[:train_size], y[:train_size]
-    X_val, y_val = X[train_size:(train_size + val_size)], y[train_size:(train_size + val_size)]
-    X_test, y_test = X[(train_size + val_size):], y[(train_size + val_size):]
-    
-    return X_train, X_val, X_test, y_train, y_val, y_test
+warnings.filterwarnings("ignore")
 
 
 def create_early_stopping(monitor:str, patience:int, restore_best_weights:bool=True, **kwargs):
     return EarlyStopping(monitor=monitor, patience=patience, restore_best_weights=restore_best_weights, **kwargs)
 
-
-def build_model(input_shape, units:int=30, activation:str|None='relu', dropout:float=0.2,
-                optimizer:str='adam', loss:str='mse', metrics:list=['mae']):
-    model = Sequential([
-        Input(shape=input_shape),
-        LSTM(units, activation=activation, return_sequences=False),
-        Dropout(dropout),
-        Dense(1)
-    ])
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-    return model
 
 def get_or_create_mlflow_experiment(experiment_name: str):
     experiment = mlflow.get_experiment_by_name(experiment_name)
@@ -55,30 +22,104 @@ def get_or_create_mlflow_experiment(experiment_name: str):
     return experiment.experiment_id
 
 
-def run_experiment(X_train, y_train, X_val, y_val, units:int, activation: str, dropout: float, optimizer: str, experiment_id: str):
+def train_val_split_lstm_feature_data(
+    X_seq: np.ndarray,
+    X_features: np.ndarray,
+    y: np.ndarray,
+    train_frac: float,
+    val_frac: float
+):
+    if train_frac + val_frac >= 1:
+        raise ValueError("train_frac + val_frac must be less than 1")
+    if not (0 < train_frac < 1) or not (0 < val_frac < 1):
+        raise ValueError("train_frac and val_frac must be between 0 and 1")
 
-    mlflow.tensorflow.autolog(log_models=True, log_datasets=False)
+    
+    train_size = int(len(X_seq) * train_frac)
+    val_size = int(len(X_seq) * val_frac)
+    
+    X_seq_train = X_seq[:train_size]
+    X_seq_val = X_seq[train_size:(train_size + val_size)]
+    X_seq_test = X_seq[(train_size + val_size):]
+    
+    X_features_train = X_features[:train_size]
+    X_features_val = X_features[train_size:(train_size + val_size)]
+    X_features_test = X_features[(train_size + val_size):]
+    
+    y_train = y[:train_size]
+    y_val = y[train_size:(train_size + val_size)]
+    y_test = y[(train_size + val_size):]
+    
+    return X_seq_train, X_seq_val, X_seq_test, X_features_train, X_features_val, X_features_test, y_train, y_val, y_test
+
+
+def build_seq_model(seq_shape, features_shape, seq_type: str, seq_units: int, dense_units: int, activation: str, 
+                dropout: float, optimizer: str, loss:str='mse', metrics:list=['mae']
+):
+    
+    # inputs 
+    seq_input = Input(shape=(seq_shape, 1))
+    features_input = Input(shape=(features_shape,))
+    
+    # seq branch
+    if seq_type == 'LSTM':
+        x_seq = LSTM(seq_units, activation=activation, return_sequences=False)(seq_input)
+    else:
+        x_seq = SimpleRNN(seq_units, activation=activation, return_sequences=False)(seq_input)    
+    x_seq = Dropout(dropout)(x_seq)
+    
+    # Dense branch 
+    x = Concatenate()([x_seq, features_input])
+    x = Dense(dense_units, activation=activation)(x)
+    x = Dropout(dropout)(x)
+    output = Dense(1)(x)
+    
+    model = Model(inputs=[seq_input, features_input], outputs=output)
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+    
+    return model
+    
+
+def run_experiment(
+    X_seq_train, X_features_train, y_train, X_seq_val, X_features_val, y_val, 
+    seq_type:str, seq_units:int, dense_units:int, activation: str, dropout: float, optimizer: str, 
+    experiment_id: str
+):
+
+    mlflow.tensorflow.autolog(log_models=True, log_datasets=False, silent=True)
 
     with mlflow.start_run(experiment_id=experiment_id):
-        early_stopping = create_early_stopping(monitor='val_loss', patience=15)
-
-        model = build_model(X_train.shape[1:], units=units, activation=activation, dropout=dropout,
-                                        optimizer=optimizer, loss='mse', metrics=['mae'])
-            
+        early_stopping = create_early_stopping(monitor='val_loss', patience=25)
+        
+        model = build_seq_model(
+            seq_shape=X_seq_train.shape[1], features_shape=X_features_train.shape[1], seq_type=seq_type,
+            seq_units=seq_units, dense_units=dense_units, activation=activation, dropout=dropout,
+            optimizer=optimizer
+        )
+                    
         _ = model.fit(
-            X_train, y_train,
-            validation_data=(X_val, y_val),
-            epochs=500,
+            [X_seq_train, X_features_train],
+            y_train,
+            validation_data=([X_seq_val, X_features_val], y_val),
+            epochs=1000,
             batch_size=64,
             callbacks=[early_stopping],
             verbose=None,
             shuffle=False
         )
-
-        val_loss = model.evaluate(X_val, y_val, verbose=0)
+        
+        val_loss = model.evaluate([X_seq_val, X_features_val], y_val)
         print(f"Validation Loss: {val_loss[0]}, Validation MAE: {val_loss[1]}")
         
-        mlflow.log_param("units", units)
+        mlflow.log_param("seq_units", seq_units)
+        mlflow.log_param("dense_units", dense_units)
         mlflow.log_param("activation", activation)
         mlflow.log_param("optimizer", optimizer)
         mlflow.log_param("dropout", dropout)
+        
+
+def get_best_model(experiment_id, metric:str="metrics.val_mae"):
+    
+    model_dfs = mlflow.search_runs(experiment_id)
+    best_run_id = model_dfs.sort_values(metric, ascending=True)['run_id'].iloc[0]
+    return mlflow.pyfunc.load_model(f"runs:/{best_run_id}/model")
